2b22d88a8102cf0179ade9aaeb01ffcc01073d76
jsfraz <josefraz@seznam.cz>
2024-07-10 22:38:55 +0200

    Connect bot to Discord.

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func main() {
 	log.SetPrefix("lada: ")
 	log.SetFlags(log.LstdFlags | log.LUTC | log.Lmicroseconds)
-	log.Println("Hello, World!")
+
+	// Create a new Discord session using the provided bot token
+	dg, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Open the websocket and begin listening.
+	err = dg.Open()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Log
+	log.Println("Láďa is running...")
+	// Wait here until CTRL-C or other term signal is received
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
 }
