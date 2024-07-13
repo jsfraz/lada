c49bec51a4d440bc4d90a14680e1b26a90e80a3e
jsfraz <josefraz@seznam.cz>
2024-07-13 23:42:36 +0200

    Fix.

--- handlers/message.go
+++ handlers/message.go
@@ -18,9 +18,11 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	// Check if message starts with bot mention
-	if !strings.HasPrefix(m.Content, s.State.User.Mention()) {
-		return
+	// Check if message starts with bot mention when in guild
+	if m.GuildID != "" {
+		if !strings.HasPrefix(m.Content, s.State.User.Mention()) {
+			return
+		}
 	}
 	// Check if the message isn't empty
 	content := strings.TrimPrefix(m.Content, s.State.User.Mention())
@@ -33,18 +35,15 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if content == "" {
 		return
 	}
-	log.Println(content)
-	// Check if message content is pong
-	if strings.ToLower(content) != "ping" {
-		return
-	}
 	// Set user mention
 	mention := ""
 	if m.GuildID != "" {
 		mention = fmt.Sprintf("%s ", m.Author.Mention())
 	}
+	// TODO generate response
+	response := content
 	// Send message
-	_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%sPong!", mention))
+	_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s%s", mention, response))
 	if err != nil {
 		log.Println(err)
 	}
